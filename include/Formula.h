fa57bae55184781ae6436b89686513b47c6323f8
Simon Uggla <simon.uggla@gmail.com>
2020-11-23 16:41:43 +0100

    Removed test code

--- include/Formula.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-class Formula {
-public:
-    static int bla(int arg1);
-};
--- include/test.h
+++
@@ -1,8 +0,0 @@
-
-namespace n3 {
-    class Vec3 {
-        public:
-            Vec3(float scalar) : x{scalar}, y{scalar}, z{scalar} {}
-            float x, y, z;
-    };
-}
