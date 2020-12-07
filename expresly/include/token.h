77ac7dbfb232c664b801e60d01ac6575a0c94001
Simon Uggla <simon.uggla@gmail.com>
2020-12-07 13:52:40 +0100

    Moved implementation to cpp file

--- expresly/include/token.h
+++ expresly/include/token.h
@@ -1,41 +1,34 @@
 #pragma once
 
 #include <string>
+
 #include "types.h"
 
 namespace Expresly {
-class Token
-{
-  public:
-    enum Type
-    {
-        Unknown = 0,
-        Number,
-        Operator,
-        Function,
-        LeftParam,
-        RightParam,
-        FunctionDelimiter
-    };
-    Token() {}
-    Token(Type t) : type{t} {}
-    Token(Type t, std::string s) : type{t}, string{s}
-    {
-        if (t == Token::Type::Number)
-        {
-            value = stod(s);
-        }
-    }
-    Token(Type t, std::string s, int prec, bool ra, FuncParam fu)
-        : type{t}, string{s}, precedence{prec}, rightAssociative(ra), function(fu) {}
+class Token {
+ public:
+  enum class Type {
+    Unknown = 0,
+    Number,
+    Operator,
+    Function,
+    LeftParam,
+    RightParam,
+    FunctionDelimiter
+  };
+  Token();
+  Token(Type);
+  Token(Type, const std::string&);
+  Token(Type, std::string&&);
+  Token(Type, const std::string&, int, bool, FuncParam);
+  Token(Type, std::string&&, int, bool, FuncParam);
 
-    Type type{Type::Unknown};
-    std::string string{};
-    int precedence{-1};
-    bool rightAssociative{false};
-    int arguments{-1};
-    FuncParam function{nullptr};
-    double value{-1};
-
+  Type type{Type::Unknown};
+  std::string string{};
+  int precedence{-1};
+  bool rightAssociative{false};
+  int arguments{-1};
+  FuncParam function{nullptr};
+  double value{-1};
 };
-}
+}  // namespace Expresly
---
+++ expresly/src/token.cpp
@@ -0,0 +1,34 @@
+#include "token.h"
+#include <string>
+
+namespace Expresly {
+
+Token::Token() {}
+Token::Token(Type type) : type{type} {}
+
+Token::Token(Type type, const std::string& expression)
+    : type{type}, string{expression} {
+  if (type == Type::Number) value = std::stod(string);
+}
+
+Token::Token(Type type, std::string&& expression)
+    : type{type}, string{std::move(expression)} {
+  if (type == Type::Number) value = std::stod(string);
+}
+
+Token::Token(Type type, const std::string& expression, int precedence,
+             bool rightAssociative, FuncParam function)
+    : type{type},
+      string{expression},
+      precedence{precedence},
+      rightAssociative{rightAssociative},
+      function{function} {}
+
+Token::Token(Type type, std::string&& expression, int precedence,
+             bool rightAssociative, FuncParam function)
+    : type{type},
+      string{std::move(expression)},
+      precedence{precedence},
+      rightAssociative{rightAssociative},
+      function{function} {}
+}  // namespace Expresly
